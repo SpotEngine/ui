f13d6709b668dd858366621ccb1cd95db434165f
hadi <m.hadi.jamshidi@gmail.com>
2025-05-30 08:29:15 +0200

    update api host

--- code/app/settings/prod.py
+++ code/app/settings/prod.py
@@ -4,7 +4,7 @@ from .conf_settings import ENVIRONMENT
 if not ENVIRONMENT == "production":
     raise Exception(f"Environment: {ENVIRONMENT} not set to production")
 
-API_HOST = "https://be1000-api.netlify.app"
+API_HOST = "https://demo-api.be1000.com"
 
 SECRET_KEY = 'iecy_=rzx0g1am(+b-gh3-t^-z^6#2s$av*6=3%$882j864u9i'
 DEBUG = False
